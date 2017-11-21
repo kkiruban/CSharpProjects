0193b363ac12eb241d448b8e59dd4c5b393ec13c
kirubanand <capkiruba26@gmail.com>
2017-11-21 20:24:38 +0530

    added

---
+++ SeleniumCSharp/SeleniumCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumCSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+
+		{
+			IWebDriver driver = new ChromeDriver();
+			driver.Navigate().GoToUrl("https://test.salesforce.com/");
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+			driver.Close();
+
+		}
+	}
+}
